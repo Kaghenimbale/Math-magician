1ce075bb03554eb229770318a242489869eb502d
Kagheni <Kaghenimbale@gmail.com>
2023-03-29 10:35:01 +0200

    Add calculate.js

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,32 +1,98 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './calculator.css';
-import Display from './Display';
+import calculate from '../logic/calculate';
 
-const Calculator = () => (
-  <div className="calculator-container">
-    <Display />
-    <div className="signs">
-      <button type="button" className="sign btn1">AC</button>
-      <button type="button" className="sign btn2">+/-</button>
-      <button type="button" className="sign btn3">%</button>
-      <button type="button" className="sign sing-math btn4">+</button>
-      <button type="button" className="sign btn5">7</button>
-      <button type="button" className="sign btn7">8</button>
-      <button type="button" className="sign btn8">9</button>
-      <button type="button" className="sign sing-math btn9">X</button>
-      <button type="button" className="sign btn10">4</button>
-      <button type="button" className="sign btn11">5</button>
-      <button type="button" className="sign btn12">6</button>
-      <button type="button" className="sign sing-math btn13">-</button>
-      <button type="button" className="sign btn14">1</button>
-      <button type="button" className="sign btn15">2</button>
-      <button type="button" className="sign btn16">3</button>
-      <button type="button" className="sign sing-math btn17">+</button>
-      <button type="button" className="sign btn18">0</button>
-      <button type="button" className="sign btn19">.</button>
-      <button type="button" className="sign sing-math btn20">=</button>
+const Calculator = () => {
+  return (
+    <div className="calculator-container">
+      <div className="input">
+        <p className="values">
+          {total}
+          {operation}
+          {next}
+        </p>
+      </div>
+      <div className="signs">
+        <button type="button" onClick={handleClick} className="sign btn1">
+          AC
+        </button>
+        <button type="button" onClick={handleClick} className="sign btn2">
+          +/-
+        </button>
+        <button type="button" onClick={handleClick} className="sign btn3">
+          %
+        </button>
+        <button
+          type="button"
+          onClick={handleClick}
+          className="sign sing-math btn4"
+        >
+          +
+        </button>
+        <button type="button" onClick={handleClick} className="sign btn5">
+          7
+        </button>
+        <button type="button" onClick={handleClick} className="sign btn7">
+          8
+        </button>
+        <button type="button" onClick={handleClick} className="sign btn8">
+          9
+        </button>
+        <button
+          type="button"
+          onClick={handleClick}
+          className="sign sing-math btn9"
+        >
+          x
+        </button>
+        <button type="button" onClick={handleClick} className="sign btn10">
+          4
+        </button>
+        <button type="button" onClick={handleClick} className="sign btn11">
+          5
+        </button>
+        <button type="button" onClick={handleClick} className="sign btn12">
+          6
+        </button>
+        <button
+          type="button"
+          onClick={handleClick}
+          className="sign sing-math btn13"
+        >
+          -
+        </button>
+        <button type="button" onClick={handleClick} className="sign btn14">
+          1
+        </button>
+        <button type="button" onClick={handleClick} className="sign btn15">
+          2
+        </button>
+        <button type="button" onClick={handleClick} className="sign btn16">
+          3
+        </button>
+        <button
+          type="button"
+          onClick={handleClick}
+          className="sign sing-math btn17"
+        >
+          +
+        </button>
+        <button type="button" onClick={handleClick} className="sign btn18">
+          0
+        </button>
+        <button type="button" onClick={handleClick} className="sign btn19">
+          .
+        </button>
+        <button
+          type="button"
+          onClick={handleClick}
+          className="sign sing-math btn20"
+        >
+          =
+        </button>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Calculator;
